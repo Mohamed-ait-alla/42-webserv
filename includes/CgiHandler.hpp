4ab48b460fb10054a05b151a08ae1b94cae0d684
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-01-30 15:45:32 +0100

    refactor(CGI): organize code in build execve's arguments function

--- includes/CgiHandler.hpp
+++ includes/CgiHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 10:43:47 by mait-all          #+#    #+#             */
-/*   Updated: 2026/01/28 10:59:22 by mait-all         ###   ########.fr       */
+/*   Updated: 2026/01/30 15:08:14 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ class CgiHandler : public Helper {
 		
 		char**	buildArguments(const Request& req);
 		char**	buildEnvVariables(const Request& req);
+		char*	getBinaryPath(const Request& req);
 		void	executeScript(int stdinReadEnd, int stdoutWriteEnd, char **argv, char **envp);
 		int		startCgiScript(const Request& req, pid_t& outPid);
 		ssize_t	readChunk(int pipeFd, char *buffer, size_t size);
--- serverCore/CgiHandler.cpp
+++ serverCore/CgiHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 10:49:39 by mait-all          #+#    #+#             */
-/*   Updated: 2026/01/29 11:47:38 by mait-all         ###   ########.fr       */
+/*   Updated: 2026/01/30 15:39:00 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,35 @@
 #include <wait.h>
 
 
+char*	CgiHandler::getBinaryPath(const Request& req)
+{
+	size_t dotPos = req.cgi.scriptPath.rfind('.');
+	if (dotPos == std::string::npos)
+		return NULL;
+
+	std::string ext = req.cgi.scriptPath.substr(dotPos);
+	std::map<std::string, std::string>::const_iterator it = req.config.cgi_conf.find(ext);
+
+	if (it == req.config.cgi_conf.end())
+		return NULL;
+
+	return strdup(it->second.c_str());
+}
+
+
 char**	CgiHandler::buildArguments(const Request& req)
 {
-	char**	argv = new char*[3];
-	std::string	extention;
-	size_t	pos  = req.cgi.scriptPath.rfind(".");
-	extention = req.cgi.scriptPath.substr(pos);
-	std::map<std::string, std::string>::const_iterator it = req.config.cgi_conf.find(extention);
-	if (it != req.config.cgi_conf.end())
-		argv[0] = strdup(it->second.c_str());
+	char* interpreter = getBinaryPath(req);
+	if (!interpreter)
+		return NULL;
 
-
+	char** argv = new char*[3];
+	argv[0] = interpreter;
 	argv[1] = strdup(req.cgi.scriptPath.c_str());
 	argv[2] = NULL;
 
-	return (argv);
+	return argv;
 }
-
 
 
 char**	CgiHandler::buildEnvVariables(const Request& req)

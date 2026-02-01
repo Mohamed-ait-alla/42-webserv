25cc98376fe8d7afb114422f3bbabd78e6e2f457
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-02-01 15:54:31 +0100

    merge code & format main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,35 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mait-all <mait-all@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mdahani <mdahani@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 16:52:42 by mdahani           #+#    #+#             */
-/*   Updated: 2026/02/01 15:13:12 by mait-all         ###   ########.fr       */
+/*   Updated: 2026/02/01 15:53:52 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/Server.hpp"
 
-int	main(int ac, char **av) {
+int main(int ac, char **av) {
 
-	try {
-		Request request;
-		Server server;
+  try {
+    Request request;
+    Server server;
 
-		if (ac <= 2)
-		{
-			char file[] = "config/default.conf";
-			request.config.parse_config_file(file);
-			if (ac == 2)
-			request.config.parse_config_file(av[1]);
-		}
-		else
-		throw std::runtime_error("./program [config file]");
+    if (ac <= 2) {
+      char file[] = "config/default.conf";
+      request.config.parse_config_file(file);
+      if (ac == 2)
+        request.config.parse_config_file(av[1]);
+    } else
+      throw std::runtime_error("./program [config file]");
 
-		debug_mode = true;
-		server.run(request);
-	} catch (const std::exception &e) {
-		std::cerr << e.what() << '\n';
-		return 1;
-	}
+    debug_mode = true;
+    server.run(request);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
 }

703ba1334b034ad1fb9c6f185346e602d34210fa
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-02-01 12:55:51 +0100

    feat(CGI): add ruby cgi script

--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-print("Content-Type: text/plain\n")
-print( "123" + ("A" * 10_000_000) + "123")
+from datetime import datetime
+
+currentDateTime = datetime.now()
+
+print("Content-Type: text/html\n")
+print()
+
+print ("<html>")
+print ("<head><title>Python CGI Test</title></head>")
+print ("<body>")
+print ("<h1>Hello from Python CGI! </h1>")
+print ("<p>If you see this, Python CGI is working!</p>")
+print (f"<p>Current time: #{currentDateTime}</p>")
+print ("</body>")
+print ("</html>")
